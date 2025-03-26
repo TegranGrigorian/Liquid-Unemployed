1eaa2963c7b43062a56596de4be555f321dfc95d
Tegran Grigorian <tegrgrigor@gmail.com>
2025-03-25 20:40:24 -0400

    liquid_unemplyed starts!

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+//This code will have the main function and pre-set disabled features becasue rust compiler is poopyy#![allow(non_snake_case)]#[allow(non_snake_case)
+#![allow(non_snake_case)]
+
+fn main() {
+    println!("FAGGOT");
+}
